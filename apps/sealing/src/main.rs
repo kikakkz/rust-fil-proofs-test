6174ea7dc5d9c1c5a0d11c43ce6554ec289700db
Zhao KK <kikakkz@hotmail.com>
2020-05-29 23:29:55 +0800

    Only add one piece due to comm_d caculation is not right

--- apps/sealing/src/main.rs
+++ apps/sealing/src/main.rs
@@ -27,15 +27,15 @@ fn main() {
 
     let _root_2k: &str = "./";
     let _src_2k: &str = "./apps/data/tt";
-    let _unseal_2k: &str = &(cache_path.to_owned() + "PPPiece.ttt.2k");
-    let _sealed_2k: &str = &(cache_path.to_owned() + "PPPiece.ttt.sealed.2k");
+    let _unseal_2k: &str = &(cache_path.to_owned() + "/PPPiece.ttt.2k");
+    let _sealed_2k: &str = &(cache_path.to_owned() + "/PPPiece.ttt.sealed.2k");
     let _size_2k: u64 = 2032;
-    let _sector_size_2k: u64 = 2048 * 8;
+    let _sector_size_2k: u64 = 2048;
 
     let _root_32gb: &str = root_target;
     let _src_32gb: &str = "s-t0121479-0";
-    let _unseal_32gb: &str = &(cache_path.to_owned() + "PPPiece.ttt");
-    let _sealed_32gb: &str = &(cache_path.to_owned() + "PPPiece.ttt.sealed");
+    let _unseal_32gb: &str = &(cache_path.to_owned() + "/PPPiece.ttt");
+    let _sealed_32gb: &str = &(cache_path.to_owned() + "/PPPiece.ttt.sealed");
     let _size_32gb: u64 = 34359738368-21-270549100+2130308-16774+131;
     let _sector_size_32gb: u64 = 32 * 1024 * 1024 * 1024;
 
@@ -46,16 +46,40 @@ fn main() {
     let sector_size: u64 = _sector_size_2k;
 
     let mut file = std::fs::File::open(src).expect("failed");
+    file.seek(SeekFrom::Start(0)).expect("fail");
     let unsealed = std::fs::File::create(unseal).expect("failed");
     let (piece_info1, _) = filecoin_proofs::add_piece(&file, &unsealed,
                                filecoin_proofs::UnpaddedBytesAmount(size),
                                &[]).expect("failed");
-    file.seek(SeekFrom::Start(0)).expect("fail");
-    let file = std::fs::File::open(src).expect("failed");
+    /* file.seek(SeekFrom::Start(0)).expect("fail");
     let (piece_info2, _) = filecoin_proofs::add_piece(&file, &unsealed,
                                filecoin_proofs::UnpaddedBytesAmount(size),
                                &[]).expect("failed");
-    let piece_infos = vec![piece_info1, piece_info2];
+    file.seek(SeekFrom::Start(0)).expect("fail");
+    let (piece_info3, _) = filecoin_proofs::add_piece(&file, &unsealed,
+                               filecoin_proofs::UnpaddedBytesAmount(size),
+                               &[]).expect("failed");
+    file.seek(SeekFrom::Start(0)).expect("fail");
+    let (piece_info4, _) = filecoin_proofs::add_piece(&file, &unsealed,
+                               filecoin_proofs::UnpaddedBytesAmount(size),
+                               &[]).expect("failed");
+    file.seek(SeekFrom::Start(0)).expect("fail");
+    let (piece_info5, _) = filecoin_proofs::add_piece(&file, &unsealed,
+                               filecoin_proofs::UnpaddedBytesAmount(size),
+                               &[]).expect("failed");
+    file.seek(SeekFrom::Start(0)).expect("fail");
+    let (piece_info6, _) = filecoin_proofs::add_piece(&file, &unsealed,
+                               filecoin_proofs::UnpaddedBytesAmount(size),
+                               &[]).expect("failed");
+    file.seek(SeekFrom::Start(0)).expect("fail");
+    let (piece_info7, _) = filecoin_proofs::add_piece(&file, &unsealed,
+                               filecoin_proofs::UnpaddedBytesAmount(size),
+                               &[]).expect("failed");
+    file.seek(SeekFrom::Start(0)).expect("fail");
+    let (piece_info8, _) = filecoin_proofs::add_piece(&file, &unsealed,
+                               filecoin_proofs::UnpaddedBytesAmount(size),
+                               &[]).expect("failed"); */
+    let piece_infos = vec![piece_info1/*, piece_info2, piece_info3, piece_info4, piece_info5, piece_info6, piece_info7, piece_info8*/];
     let _sealed = std::fs::File::create(sealed).expect("failed");
 
     let config = PoRepConfig {
